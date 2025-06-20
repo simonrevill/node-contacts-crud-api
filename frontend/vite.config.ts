f04b7b399b6182a9e68884d20763b6830f83b622
Simon Revill <simon.revill@vertumotors.com>
2025-06-20 21:15:13 +0100

    add frontend unit testing deps

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,8 +1,14 @@
+/// <reference types="vitest/config" />
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tsconfigPaths from "vite-tsconfig-paths";
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tsconfigPaths()],
+  test: {
+    globals: true,
+    environment: "jsdom",
+    setupFiles: "./vitest.setup.ts",
+    reporters: ["verbose"],
+  },
 });
