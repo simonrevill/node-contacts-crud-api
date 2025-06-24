70fbdfdd7c07c494aa94bff05cc021a2eec1cd65
Simon Revill <simon.revill@vertumotors.com>
2025-06-24 10:27:10 +0100

    update spec file describe and test names

--- frontend/e2e/1-initial-load.spec.ts
+++ frontend/e2e/1-initial-load.spec.ts
@@ -6,14 +6,18 @@ beforeEach(async ({ page }) => {
   await page.goto("/");
 });
 
-describe("initial page load", () => {
-  test("page has correct title", async ({ page }) => {
+describe("Initial load of the application", () => {
+  test("User sees the correct page title when the application loads", async ({
+    page,
+  }) => {
     // Assert
     await expect(page).toHaveTitle(/Contact Manager/);
   });
 
-  test("page has correct heading", async ({ page }) => {
-    // Act
+  test("User sees the correct heading when the application loads", async ({
+    page,
+  }) => {
+    // Arrange
     const header = page.getByRole("banner");
     const heading = header.getByRole("heading", {
       level: 1,
