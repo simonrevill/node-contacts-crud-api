5be935e621c7f0fd45dac48616dfbffb81c4ccf5
Simon Revill <simon.revill@vertumotors.com>
2025-06-27 17:04:36 +0100

    add header styles

--- frontend/src/contacts/components/Header/Header.tsx
+++ frontend/src/contacts/components/Header/Header.tsx
@@ -1,7 +1,11 @@
+import { Box, Heading } from "@chakra-ui/react";
+
 export default function Header() {
   return (
-    <header>
-      <h1>Contacts Manager</h1>
-    </header>
+    <Box as="header" p={4}>
+      <Heading as="h1" size="xl">
+        Contacts Manager
+      </Heading>
+    </Box>
   );
 }
