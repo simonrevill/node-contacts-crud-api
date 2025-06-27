b6f657528076230a57c02cc5b6591426fb4e29ba
Simon Revill <simon.revill@vertumotors.com>
2025-06-27 10:24:10 +0100

    fix: await fetchContacts call in test again

--- frontend/tests/contacts/api/ContactsApiService.test.ts
+++ frontend/tests/contacts/api/ContactsApiService.test.ts
@@ -13,7 +13,7 @@ describe("API service adapter tests", () => {
     const { fetchContacts } = createContactsApiAdapter({ request: spy });
 
     // Assert
-    expect(async () => await fetchContacts()).rejects.toMatchObject({
+    await expect(fetchContacts()).rejects.toMatchObject({
       status: 500,
       error: "Something went wrong.",
     });
