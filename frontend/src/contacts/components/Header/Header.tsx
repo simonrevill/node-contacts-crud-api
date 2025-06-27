5cc058404f91e9e36ea34e9194a3f59591cc9830
Simon Revill <simon.revill@vertumotors.com>
2025-06-27 17:25:40 +0100

    frontend: add header styles

--- frontend/src/contacts/components/Header/Header.tsx
+++ frontend/src/contacts/components/Header/Header.tsx
@@ -1,9 +1,19 @@
-import { Box, Heading } from "@chakra-ui/react";
+import { Box, IconButton, Heading } from "@chakra-ui/react";
+import { Menu } from "lucide-react";
 
 export default function Header() {
   return (
-    <Box as="header" p={4}>
-      <Heading as="h1" size="xl">
+    <Box as="header" p={4} display="flex" alignItems="center" gap={4}>
+      <IconButton
+        aria-label="Menu"
+        variant="ghost"
+        aria-expanded="false"
+        w={11}
+        h={11}
+      >
+        <Menu size={24} />
+      </IconButton>
+      <Heading as="h1" size="xl" fontWeight="bold">
         Contacts Manager
       </Heading>
     </Box>
--- frontend/tests/contacts/components/Header.test.tsx
+++ frontend/tests/contacts/components/Header.test.tsx
@@ -1,11 +1,13 @@
-import { render, screen, within } from "@testing-library/react";
+import { screen, within } from "@testing-library/react";
 import { Header } from "../../../src/contacts/components/Header";
+import { renderWithChakraProvider } from "../test-utils";
 
 describe("Header component tests", () => {
   it("should render the correct heading", () => {
     // Arrange
-    render(<Header />);
+    renderWithChakraProvider(<Header />);
 
+    screen.debug();
     // Assert
     const header = screen.getByRole("banner");
     const heading = within(header).getByRole("heading", {
@@ -13,5 +15,18 @@ describe("Header component tests", () => {
       name: /Contacts Manager/i,
     });
     expect(heading).toBeVisible();
+  });
+
+  it("should render a menu button to toggle the menu open and closed state", () => {
+    // Arrange
+    renderWithChakraProvider(<Header />);
+
+    // Assert
+    const header = screen.getByRole("banner");
+    const menuButton = within(header).getByRole("button", {
+      name: /Menu/i,
+    });
+    expect(menuButton).toBeVisible();
+    expect(menuButton).toHaveAttribute("aria-expanded", "false");
   });
 });
