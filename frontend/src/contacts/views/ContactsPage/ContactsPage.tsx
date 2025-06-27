4fb04da9bd1337a1adc79af7adfe75ebdd744669
Simon Revill <simon.revill@vertumotors.com>
2025-06-27 17:43:26 +0100

    frontend: add basic contact list styles

--- frontend/src/contacts/views/ContactsPage/ContactsPage.tsx
+++ frontend/src/contacts/views/ContactsPage/ContactsPage.tsx
@@ -1,3 +1,4 @@
+import { Box, List, Text } from "@chakra-ui/react";
 import { NoContacts } from "../../components";
 import useContacts from "../../hooks/useContacts";
 
@@ -6,22 +7,48 @@ export default function ContactsPage() {
 
   if (isError) {
     return (
-      <div role="alert">
-        <h2>Something went wrong.</h2>
-        <p>Please try again later</p>
-      </div>
+      <Box as="main" p={4}>
+        <div role="alert">
+          <h2>Something went wrong.</h2>
+          <p>Please try again later</p>
+        </div>
+      </Box>
     );
   }
 
   if (hasNoContacts) {
-    return <NoContacts />;
+    return (
+      <Box as="main" p={4}>
+        <NoContacts />
+      </Box>
+    );
   }
 
   return (
-    <ul>
-      {contacts.map((contact) => (
-        <li key={contact.id}>{`${contact.firstName} ${contact.lastName}`}</li>
-      ))}
-    </ul>
+    <Box as="main" p={4}>
+      <List.Root
+        as="ul"
+        listStyle="none"
+        borderWidth={0.25}
+        borderStyle="solid"
+        borderColor="gray.300"
+        borderRadius={2}
+      >
+        {contacts.map((contact) => (
+          <List.Item
+            key={contact.id}
+            p={4}
+            borderBottomWidth={0.25}
+            borderBottomStyle="solid"
+            borderBottomColor="gray.300"
+            _last={{ borderBottom: "none" }}
+          >
+            <Text as="span" fontSize="md" fontWeight="semibold">
+              {`${contact.firstName} ${contact.lastName}`}
+            </Text>
+          </List.Item>
+        ))}
+      </List.Root>
+    </Box>
   );
 }
