261c2ef43595fe716ed98633fa8f22432e4e8d30
Simon Revill <simon.revill@vertumotors.com>
2025-06-30 12:44:11 +0100

    e2e: User can navigate back to the contacts page from the add contact page using the cancel button

--- frontend/e2e/contacts/2-creating-contacts.spec.ts
+++ frontend/e2e/contacts/2-creating-contacts.spec.ts
@@ -18,7 +18,7 @@ describe("Creating contacts", () => {
     ).toBeVisible();
   });
 
-  test("User can navigate back to the contacts page from the add contact page", async ({
+  test("User can navigate back to the contacts page from the add contact page using the back button", async ({
     contactsPage,
     page,
   }) => {
@@ -27,6 +27,24 @@ describe("Creating contacts", () => {
     await page.waitForURL("**/create");
 
     const backButton = await page.getByRole("button", { name: /Back/i });
+
+    // Act
+    await backButton.click();
+    await page.waitForURL("/");
+
+    // Assert
+    await expect(contactsPage.contactListHeading).toBeVisible();
+  });
+
+  test("User can navigate back to the contacts page from the add contact page using the cancel button", async ({
+    contactsPage,
+    page,
+  }) => {
+    // Arrange
+    await contactsPage.goToNewContactForm();
+    await page.waitForURL("**/create");
+
+    const backButton = await page.getByRole("button", { name: /Cancel/i });
 
     // Act
     await backButton.click();
