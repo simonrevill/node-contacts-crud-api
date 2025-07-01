a0d32ed61e3d01dfb2c20fff243bf87795d754e7
Simon Revill <simon.revill@vertumotors.com>
2025-07-01 11:59:28 +0100

    frontend: should be called with the correct URL and send the correct request

--- frontend/tests/contacts/useCases/createContact.test.ts
+++ frontend/tests/contacts/useCases/createContact.test.ts
@@ -7,6 +7,38 @@ import { createContact } from "../../../src/contacts/useCases";
 import { createMockContact, createMockContactResponse } from "../test-utils";
 
 describe("Creating contacts use case", async () => {
+  it("should be called with the correct URL and send the correct request", async () => {
+    // Arrange
+    const newContact: ContactInput = createMockContact();
+    const expectedNewContactResponse: Contact =
+      createMockContactResponse(newContact);
+    const expectedLocation = `/api/contacts/${expectedNewContactResponse.id}`;
+    const spy = vi.fn().mockResolvedValue({
+      ok: true,
+      status: 201,
+      headers: {
+        get: (name: string) => (name === "location" ? expectedLocation : null),
+      },
+      json: () => expectedNewContactResponse,
+    });
+    const fakeApi = createContactsApiAdapter({ request: spy });
+
+    // Act
+    await createContact(fakeApi, newContact);
+
+    // Assert
+    expect(spy).toHaveBeenCalledWith(
+      "http://localhost:8080/api/contacts",
+      expect.objectContaining({
+        method: "POST",
+        headers: expect.objectContaining({
+          "Content-Type": "application/json",
+        }),
+        body: JSON.stringify(newContact),
+      })
+    );
+  });
+
   it("should create and return a new contact with a 201 status and the correct location header", async () => {
     // Arrange
     const newContact: ContactInput = createMockContact();
